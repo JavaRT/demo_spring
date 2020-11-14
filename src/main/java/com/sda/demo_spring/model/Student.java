7e8968a095cb801bd90a5ad8b59a7b9060630d8b
Artiom U <arti.urb@gmail.com>
2020-11-14 15:50:36 +0100

    com 2

--- src/main/java/com/sda/demo_spring/model/Student.java
+++ src/main/java/com/sda/demo_spring/model/Student.java
@@ -3,6 +3,7 @@ package com.sda.demo_spring.model;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.Formula;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -20,6 +21,7 @@ public class Student {
     private String lastName;
     private boolean graduated;
     private Double homeDistance;
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
     private LocalDate birthDate;
     // birthDate jest nazwą kolumny w bazie. Czasami baza nazywa kolumny z podkreślnikami i w tej sytuacji możemy napotkać problemy.
     @Formula("(year(now())-year(birth_date))")
