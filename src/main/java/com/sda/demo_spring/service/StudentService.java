41a7428a38283fde2db78100cc3272e36d446995
Artiom U <arti.urb@gmail.com>
2020-11-15 13:23:54 +0100

    com 6

--- src/main/java/com/sda/demo_spring/service/StudentService.java
+++ src/main/java/com/sda/demo_spring/service/StudentService.java
@@ -3,15 +3,22 @@ package com.sda.demo_spring.service;
 import com.sda.demo_spring.model.Student;
 import com.sda.demo_spring.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+// Bean to jest instancja klasy która jest singletonem w naszym projekcie. Instancja tworzona jest przy starcie aplikacji lub w dowolnym momencie działąnia.
+//  - na bean składa się: instancja + jej typ + jej nazwa
 @Service
-@RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> findAll() {
         return studentRepository.findAll();
