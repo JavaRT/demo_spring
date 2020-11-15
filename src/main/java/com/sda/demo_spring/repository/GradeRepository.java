61f7a64477c35f4aa61ce671e13397b142b26e29
Artiom U <arti.urb@gmail.com>
2020-11-15 14:01:00 +0100

    com 7

---
+++ src/main/java/com/sda/demo_spring/repository/GradeRepository.java
@@ -0,0 +1,9 @@
+package com.sda.demo_spring.repository;
+
+import com.sda.demo_spring.model.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+}
---
+++ src/main/java/com/sda/demo_spring/service/GradeService.java
@@ -0,0 +1,25 @@
+package com.sda.demo_spring.service;
+
+import com.sda.demo_spring.model.Grade;
+import com.sda.demo_spring.model.Student;
+import com.sda.demo_spring.repository.GradeRepository;
+import com.sda.demo_spring.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GradeService {
+    private final GradeRepository gradeRepository;
+    private final StudentRepository studentRepository;
+
+    public Optional<Student> findById(Long studentId) {
+        return studentRepository.findById(studentId);
+    }
+
+    public void save(Grade grade) {
+        gradeRepository.save(grade);
+    }
+}
